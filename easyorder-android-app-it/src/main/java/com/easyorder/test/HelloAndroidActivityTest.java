37e4a1cbea264925062e4f535e2bcc735a9c9b54
Bing-Qian Luan <bingqian.luan@gmail.com>
2013-07-08 21:57:05 +0800

    create project

---
+++ easyorder-android-app-it/src/main/java/com/easyorder/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package com.easyorder.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.easyorder.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
